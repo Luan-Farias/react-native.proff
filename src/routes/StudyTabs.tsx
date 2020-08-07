e44a72878669470ff07c5364dc51fcd028391a8f
Luan Farias <luan.farias_bvs@outlook.com>
2020-08-07 19:00:28 -0300

    The Page Header was maden

--- src/routes/StudyTabs.tsx
+++ src/routes/StudyTabs.tsx
@@ -42,7 +42,7 @@ const StudyTabs: React.FC = () => {
                 component={TeacherList}
                 options={{
                     tabBarLabel: 'Proffys',
-                    tabBarIcon: ({ color, size }) => (<Ionicons name="ios-easel" size={size} color={color} />),
+                    tabBarIcon: ({ color, size, focused }) => (<Ionicons name="ios-easel" size={size} color={focused ? '#8257E5' : color} />),
                 }}
             />
             <Screen
@@ -50,7 +50,7 @@ const StudyTabs: React.FC = () => {
                 component={Favorites}
                 options={{
                     tabBarLabel: 'Favoritos',
-                    tabBarIcon: ({ color, size }) => (<Ionicons name="ios-heart" size={size} color={color} />),
+                    tabBarIcon: ({ color, size, focused }) => (<Ionicons name="ios-easel" size={size} color={focused ? '#8257E5' : color} />),
                 }}
             />
         </Navigator>
