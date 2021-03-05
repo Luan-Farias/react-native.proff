bc77a56de4ebd4d433cc04ded6d09417a118edbc
Luan Farias <luan.farias_bvs@outlook.com>
2021-03-05 17:31:22 -0300

    maden the api connection and lading page is connecting with the api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { View, Image, Text } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
+import React from "react";
+import { View, Image, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 
-import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
-import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
-import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+import heartOutlineIcon from "../../assets/images/icons/heart-outline.png";
+import unfavoriteIcon from "../../assets/images/icons/unfavorite.png";
+import whatsappIcon from "../../assets/images/icons/whatsapp.png";
 
-import styles from './styles';
+import styles from "./styles";
 
 const TeacherItem: React.FC = () => {
     return (
@@ -14,7 +14,10 @@ const TeacherItem: React.FC = () => {
             <View style={styles.profile}>
                 <Image
                     style={styles.avatar}
-                    source={{ uri: 'https://avatars1.githubusercontent.com/u/60125300?s=460&u=98eeb4a4d64bc31f638be7acae2f773f152b27f9&v=4' }}
+                    source={{
+                        uri:
+                            "https://avatars1.githubusercontent.com/u/60125300?s=460&u=98eeb4a4d64bc31f638be7acae2f773f152b27f9&v=4",
+                    }}
                 />
                 <View style={styles.profileInfo}>
                     <Text style={styles.name}>Luan Farias</Text>
@@ -24,20 +27,23 @@ const TeacherItem: React.FC = () => {
 
             <Text style={styles.bio}>
                 Entusiastas das melhores tecnologias de química avanãda.
-                {'\n'}{'\n'}
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas atráves de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                {"\n"}
+                {"\n"}
+                Apaixonado por explodir coisas em laboratório e por mudar a vida
+                das pessoas atráves de experiências. Mais de 200.000 pessoas já
+                passaram por uma das minhas explosões.
             </Text>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
-                    Preço/Hora {'   '}
-                    <Text style={styles.priceValue}>
-                        R$ 20,00
-                    </Text>
+                    Preço/Hora {"   "}
+                    <Text style={styles.priceValue}>R$ 20,00</Text>
                 </Text>
 
                 <View style={styles.buttonsContainer}>
-                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
+                    <RectButton
+                        style={[styles.favoriteButton, styles.favorited]}
+                    >
                         {/* <Image source={heartOutlineIcon} /> */}
                         <Image source={unfavoriteIcon} />
                     </RectButton>
@@ -51,6 +57,6 @@ const TeacherItem: React.FC = () => {
             </View>
         </View>
     );
-}
+};
 
 export default TeacherItem;
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import { View, Image, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { RectButton } from 'react-native-gesture-handler';
+import React, { useEffect, useState } from "react";
+import { View, Image, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { RectButton } from "react-native-gesture-handler";
 
-import styles from './styles';
+import styles from "./styles";
 
-import landingImg from '../../assets/images/landing.png';
-import studyIcon from '../../assets/images/icons/study.png';
-import giveClassesIcon from '../../assets/images/icons/give-classes.png';
-import heartIcon from '../../assets/images/icons/heart.png';
+import landingImg from "../../assets/images/landing.png";
+import studyIcon from "../../assets/images/icons/study.png";
+import giveClassesIcon from "../../assets/images/icons/give-classes.png";
+import heartIcon from "../../assets/images/icons/heart.png";
+import api from "../../services/api";
 
-function Landing () {
+function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get("/connections").then((response) => {
+            const { total } = response.data;
+
+            setTotalConnections(total);
+        });
+    }, []);
+
     const { navigate } = useNavigation();
 
-    function handleNavigateToGiveClassesPage () {
-        navigate('GiveClasses');
+    function handleNavigateToGiveClassesPage() {
+        navigate("GiveClasses");
     }
 
-    function handleNavigateToStudyPages () {
-        navigate('Study');
+    function handleNavigateToStudyPages() {
+        navigate("Study");
     }
 
     return (
@@ -26,14 +37,14 @@ function Landing () {
             <Image source={landingImg} style={styles.banner} />
 
             <Text style={styles.title}>
-                Seja bem-vindo, {'\n'}
+                Seja bem-vindo, {"\n"}
                 <Text style={styles.titleBold}>O que deseja fazer?</Text>
             </Text>
 
             <View style={styles.buttonsContainer}>
                 <RectButton
-                onPress={handleNavigateToStudyPages}
-                style={[ styles.button, styles.buttonPrimary ]}
+                    onPress={handleNavigateToStudyPages}
+                    style={[styles.button, styles.buttonPrimary]}
                 >
                     <Image source={studyIcon} />
 
@@ -42,8 +53,7 @@ function Landing () {
 
                 <RectButton
                     onPress={handleNavigateToGiveClassesPage}
-                    style={[ styles.button, styles.buttonSecondary ]}
-
+                    style={[styles.button, styles.buttonSecondary]}
                 >
                     <Image source={giveClassesIcon} />
 
@@ -52,7 +62,7 @@ function Landing () {
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 200 conexões realizadas {" "}
+                Total de {totalConnections} conexões realizadas{" "}
                 <Image source={heartIcon} />
             </Text>
         </View>
